01e4097d7571aef4b2cd346ecba4b558ed6927b5
Julien de Castelnau <tripod320@gmail.com>
2021-10-29 16:31:02 -0500

    partial support for multi var output

--- out.rkt
+++ out.rkt
@@ -15,31 +15,61 @@
 
 ;;; STRUCT DECL SECTION
 
+; Start nonterminal
+(struct Struct_$eval (s.t1))
+; S nonterminal
+(struct Struct_$pass ())
+(struct Struct_$throw ())
+(struct Struct_$assign-x (e.t1))
+(struct Struct_$assign-y (e.t1))
+(struct Struct_$assign-c (e.t1))
+(struct Struct_$cons (s.t1 s.t2))
+(struct Struct_$while (b.t1 s.t1))
+(struct Struct_$ite (b.t1 s.t1 s.t2))
 ; E nonterminal
 (struct Struct_$x ())
 (struct Struct_$y ())
+(struct Struct_$c ())
 (struct Struct_$0 ())
 (struct Struct_$1 ())
-(struct Struct_$+ (et1 et2))
-(struct Struct_$ite (bt1 et1 et2))
+(struct Struct_$+ (e.t1 e.t2))
+(struct Struct_$- (e.t1 e.t2))
+(struct Struct_$ite (b.t1 e.t1 e.t2))
 ; B nonterminal
 (struct Struct_$true ())
 (struct Struct_$false ())
-(struct Struct_$not (bt1))
-(struct Struct_$and (bt1 bt2))
-(struct Struct_$lt (et1 et2))
+(struct Struct_$not (b.t1))
+(struct Struct_$and (b.t1 b.t2))
+(struct Struct_$lt (e.t1 e.t2))
 
 ;;; SYNTAX SECTION
 
-(current-grammar-depth 2)
+(current-grammar-depth 3)
 (define-grammar (gram)
+  [Start
+    (choose
+      (Struct_$eval (S))
+    )]
+  [S
+    (choose
+      (Struct_$pass)
+      (Struct_$throw)
+      (Struct_$assign-x (E))
+      (Struct_$assign-y (E))
+      (Struct_$assign-c (E))
+      (Struct_$cons (S) (S))
+      (Struct_$while (B) (S))
+      (Struct_$ite (B) (S) (S))
+    )]
   [E
     (choose
       (Struct_$x)
       (Struct_$y)
+      (Struct_$c)
       (Struct_$0)
       (Struct_$1)
       (Struct_$+ (E) (E))
+      (Struct_$- (E) (E))
       (Struct_$ite (B) (E) (E))
     )]
   [B
@@ -54,27 +84,42 @@
 
 ;;; SEMANTICS SECTION
 
-(define (E.Sem p x y)
+(define (Start.Sem p x0 y0)
   (destruct p
-    [(Struct_$x) x]
-    [(Struct_$y) y]
+    [(Struct_$eval s.t1) (begin(define c0 0) (define x2 (S.Sem s.t1 x0 y0 c0)))]))
+(define (S.Sem p x0 y0 c0)
+  (destruct p
+    [(Struct_$pass) (begin (list x0 y0 c0))]
+    [(Struct_$throw) (begin UNIMPLEMENTED (list x0 y0 c0))]
+    [(Struct_$assign-x e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (list e.v1 y0 c0))]
+    [(Struct_$assign-y e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (list x0 e.v1 c0))]
+    [(Struct_$assign-c e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (list x0 y0 e.v1))]
+    [(Struct_$cons s.t1 s.t2) (begin (define x1 (S.Sem s.t1 x0 y0 c0)) (define x2 (S.Sem s.t2 x1 y1 c1)) (list   ))]
+    [(Struct_$while b.t1 s.t1) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) UNIMPLEMENTED (define x1 (S.Sem s.t1 x0 y0 c0)) (define x2 (S.Sem s.t x1 y1 c1)) (list   ))]
+    [(Struct_$ite b.t1 s.t1 s.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) UNIMPLEMENTED (define x2 (S.Sem s.t1 x0 y0 c0)) (list   ))]))
+(define (E.Sem p x0 y0 c0)
+  (destruct p
+    [(Struct_$x) x0]
+    [(Struct_$y) y0]
+    [(Struct_$c) c0]
     [(Struct_$0) 0]
     [(Struct_$1) 1]
-    [(Struct_$+ et1 et2) (begin (define iv1 (E.Sem et1 x y)) (define iv2 (E.Sem et2 x y)) (+ iv1 iv2))]
-    [(Struct_$ite bt1 et1 et2) (begin (define bv1 (B.Sem bt1 x y)) (define iv1 (E.Sem et1 x y)) (define iv2 (E.Sem et2 x y)) (ite bv1 iv1 iv2))]))
-(define (B.Sem p x y)
+    [(Struct_$+ e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(+ e.v1 e.v2))]
+    [(Struct_$- e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(- e.v1 e.v2))]
+    [(Struct_$ite b.t1 e.t1 e.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(ite b.v1 e.v1 e.v2))]))
+(define (B.Sem p x0 y0 c0)
   (destruct p
     [(Struct_$true) true]
     [(Struct_$false) false]
-    [(Struct_$not bt1) (begin (define bv1 (B.Sem bt1 x y)) bv1)]
-    [(Struct_$and bt1 bt2) (begin (define bv1 (B.Sem bt1 x y)) (define bv2 (B.Sem bt2 x y)) (and bv1 bv2))]
-    [(Struct_$lt et1 et2) (begin (define iv1 (E.Sem et1 x y)) (define iv2 (E.Sem et2 x y)) (< iv1 iv2))]))
+    [(Struct_$not b.t1) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0))b.v1)]
+    [(Struct_$and b.t1 b.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) (define b.v2 (B.Sem b.t2 x0 y0 c0))(and b.v1 b.v2))]
+    [(Struct_$lt e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(< e.v1 e.v2))]))
 
 ;;; CONSTRAINTS SECTION
 
 (define (sol) (gram))
-(define sol_E
+(define sol_Start
 (synthesize
 #:forall (list)
-#:guarantee (assert (and (equal? (E.Sem (sol) 4 2) 4) (equal? (E.Sem (sol) 2 5) 5)))))
-(print-forms sol_E)
+#:guarantee (assert (and (equal? (Start.Sem (sol) 0 0) 0) (equal? (Start.Sem (sol) 0 1) 0) (equal? (Start.Sem (sol) 1 1) 1) (equal? (Start.Sem (sol) 3 5) 15) (equal? (Start.Sem (sol) 7 2) 14) (equal? (Start.Sem (sol) 5 8) 40)))))
+(print-forms sol_Start)
--- src/RosetteSolver/SemanticsBuilder.cs
+++ src/RosetteSolver/SemanticsBuilder.cs
@@ -1,18 +1,26 @@
 using Semgus.Util;
 using Semgus.Interpreter;
 using Semgus.Syntax;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Semgus.Solver.Rosette {
     class SemanticsBuilder {
         private static readonly CodeTextBuilder _builder = new CodeTextBuilder();
         private readonly static int DEPTH = 4;
 
-        private static void GenerateRuleSteps(IInterpretationStep s) {
+        private static void GenerateRuleSteps(IInterpretationStep s, Dictionary<VariableInfo,int> outvars, string[] outvals) {
             switch(s) {
                 case AssignmentFromLocalFormula a_s:
-                    _builder.Write($"{a_s.Expression.Formula.PrintFormula()}");
+                    int index;
+                    if ((index = (outvars.GetValueOrDefault(a_s.ResultVar, -1))) != -1) {
+                        outvals[index] = a_s.Expression.Formula.PrintFormula();
+                    } else {
+                        _builder.Write($"(define {a_s.ResultVar.Name} {a_s.Expression.Formula.PrintFormula()})");
+                    } 
                     break;
                 case TermEvaluation c_s:
+                    _builder.Write(" ");
                     using (_builder.InParens()) {
                         _builder.Write("define ");
                         _builder.Write($"{c_s.OutputVariables[0].Name} ");
@@ -27,9 +35,20 @@ namespace Semgus.Solver.Rosette {
                 case ConditionalAssertion:
                     // unimplemented for now but will be added soon, fall thru for now
                 default:
-                    _builder.Write("UNIMPLEMENTED RULE INTERPRETER STEP");
+                    _builder.Write(" UNIMPLEMENTED");
                     break;
+            }
+        }
 
+        private static void GenerateOutputStatement(string[] outvals) {
+            if (outvals.Count() == 1) {
+                _builder.Write(outvals[0]);
+            } else {
+                _builder.Write(" ");
+                using (_builder.InParens()) {
+                    _builder.Write("list ");
+                    _builder.WriteEach(outvals);
+                }
             }
         }
         public static string BuildSemGenFns(InterpretationGrammar g) {
@@ -37,6 +56,15 @@ namespace Semgus.Solver.Rosette {
             _builder.Write("\n;;; SEMANTICS SECTION\n");
             
             foreach (Nonterminal nt in g.Nonterminals) {
+                Dictionary<VariableInfo, int> outvars = new Dictionary<VariableInfo, int>();
+                
+                // this is plain stupid
+                int i = 0;
+                foreach (VariableInfo o in g.Productions[nt][0].OutputVariables) {
+                    outvars.Add(o, i);
+                    i++;
+                }
+
                 _builder.LineBreak();
                 using (_builder.InParens()) {
                     _builder.Write($"define ({nt.Name}.Sem p");
@@ -52,6 +80,8 @@ namespace Semgus.Solver.Rosette {
                     using (_builder.InParens()) {
                         _builder.Write("destruct p");
                         foreach (ProductionRuleInterpreter pi in g.Productions[nt]) {
+                            string[] outvals = new string[pi.OutputVariables.Count];
+
                             _builder.LineBreak();
                             using (_builder.InBrackets()) {
                                 using (_builder.InParens()) {
@@ -63,14 +93,15 @@ namespace Semgus.Solver.Rosette {
                                 _builder.Write(" ");
                                 // hack for atomic productions
                                 if (pi.Semantics[0].Steps.Count == 1) {
-                                    GenerateRuleSteps(pi.Semantics[0].Steps[0]);
+                                    GenerateRuleSteps(pi.Semantics[0].Steps[0], outvars, outvals);
+                                    GenerateOutputStatement(outvals);
                                 } else {
                                     using (_builder.InParens()) {
                                         _builder.Write("begin");
-                                        foreach (IInterpretationStep step in pi.Semantics[0].Steps) {
-                                            _builder.Write(" ");
-                                            GenerateRuleSteps(step);
+                                        foreach (IInterpretationStep step in pi.Semantics[0].Steps) {   
+                                            GenerateRuleSteps(step, outvars, outvals);
                                         }
+                                        GenerateOutputStatement(outvals);
                                     }
                                 }
                                 
