f72d0beeeee1aa9eb1ee8d6791f3babc91fc5bca
Julien de Castelnau <tripod320@gmail.com>
2021-11-01 17:11:56 -0500

    partial imperative benchmark support

--- out.rkt
+++ out.rkt
@@ -86,34 +86,38 @@
 
 (define (Start.Sem p x0 y0)
   (destruct p
-    [(Struct_$eval s.t1) (begin(define c0 0) (define x2 (S.Sem s.t1 x0 y0 c0)))]))
+    [(Struct_$eval s.t1) (begin (define c0 0) (define ( x2 y2 c2) (S.Sem s.t1 x0 y0 c0)) c2)]))
 (define (S.Sem p x0 y0 c0)
   (destruct p
-    [(Struct_$pass) (begin (list x0 y0 c0))]
-    [(Struct_$throw) (begin UNIMPLEMENTED (list x0 y0 c0))]
-    [(Struct_$assign-x e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (list e.v1 y0 c0))]
-    [(Struct_$assign-y e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (list x0 e.v1 c0))]
-    [(Struct_$assign-c e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (list x0 y0 e.v1))]
-    [(Struct_$cons s.t1 s.t2) (begin (define x1 (S.Sem s.t1 x0 y0 c0)) (define x2 (S.Sem s.t2 x1 y1 c1)) (list   ))]
-    [(Struct_$while b.t1 s.t1) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) UNIMPLEMENTED (define x1 (S.Sem s.t1 x0 y0 c0)) (define x2 (S.Sem s.t x1 y1 c1)) (list   ))]
-    [(Struct_$ite b.t1 s.t1 s.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) UNIMPLEMENTED (define x2 (S.Sem s.t1 x0 y0 c0)) (list   ))]))
+    [(Struct_$pass) (begin  (values x0 y0 c0))]
+    [(Struct_$throw) (begin  (values x0 y0 c0))]
+    [(Struct_$assign-x e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0))  (values e.v1 y0 c0))]
+    [(Struct_$assign-y e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0))  (values x0 e.v1 c0))]
+    [(Struct_$assign-c e.t1) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0))  (values x0 y0 e.v1))]
+    [(Struct_$cons s.t1 s.t2) (begin (define ( x1 y1 c1) (S.Sem s.t1 x0 y0 c0)) (define ( x2 y2 c2) (S.Sem s.t2 x1 y1 c1))  (values x2 y2 c2))]
+    [(Struct_$while b.t1 s.t1)  (define b.v1 (B.Sem b.t1 x0 y0 c0))    (if (b.v1)
+        ((begin(define b.v1 (B.Sem b.t1 x0 y0 c0))(define ( x1 y1 c1) (S.Sem s.t1 x0 y0 c0))(define ( x2 y2 c2) (S.Sem s.t x1 y1 c1))  (values x2 y2 c2)))
+        ((begin(define b.v1 (B.Sem b.t1 x0 y0 c0))  (values x0 y0 c0))))]
+    [(Struct_$ite b.t1 s.t1 s.t2)  (define b.v1 (B.Sem b.t1 x0 y0 c0))    (if (b.v1)
+        ((begin(define b.v1 (B.Sem b.t1 x0 y0 c0))(define ( x2 y2 c2) (S.Sem s.t1 x0 y0 c0))  (values x2 y2 c2)))
+        ((begin(define b.v1 (B.Sem b.t1 x0 y0 c0))(define ( x2 y2 c2) (S.Sem s.t2 x0 y0 c0))  (values x2 y2 c2))))]))
 (define (E.Sem p x0 y0 c0)
   (destruct p
-    [(Struct_$x) x0]
-    [(Struct_$y) y0]
-    [(Struct_$c) c0]
-    [(Struct_$0) 0]
-    [(Struct_$1) 1]
-    [(Struct_$+ e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(+ e.v1 e.v2))]
-    [(Struct_$- e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(- e.v1 e.v2))]
-    [(Struct_$ite b.t1 e.t1 e.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(ite b.v1 e.v1 e.v2))]))
+    [(Struct_$x)  x0]
+    [(Struct_$y)  y0]
+    [(Struct_$c)  c0]
+    [(Struct_$0)  0]
+    [(Struct_$1)  1]
+    [(Struct_$+ e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0)) (+ e.v1 e.v2))]
+    [(Struct_$- e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0)) (- e.v1 e.v2))]
+    [(Struct_$ite b.t1 e.t1 e.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0)) (ite b.v1 e.v1 e.v2))]))
 (define (B.Sem p x0 y0 c0)
   (destruct p
-    [(Struct_$true) true]
-    [(Struct_$false) false]
-    [(Struct_$not b.t1) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0))b.v1)]
-    [(Struct_$and b.t1 b.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) (define b.v2 (B.Sem b.t2 x0 y0 c0))(and b.v1 b.v2))]
-    [(Struct_$lt e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0))(< e.v1 e.v2))]))
+    [(Struct_$true)  true]
+    [(Struct_$false)  false]
+    [(Struct_$not b.t1) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) b.v1)]
+    [(Struct_$and b.t1 b.t2) (begin (define b.v1 (B.Sem b.t1 x0 y0 c0)) (define b.v2 (B.Sem b.t2 x0 y0 c0)) (and b.v1 b.v2))]
+    [(Struct_$lt e.t1 e.t2) (begin (define e.v1 (E.Sem e.t1 x0 y0 c0)) (define e.v2 (E.Sem e.t2 x0 y0 c0)) (< e.v1 e.v2))]))
 
 ;;; CONSTRAINTS SECTION
 
--- src/RosetteSolver/SemanticsBuilder.cs
+++ src/RosetteSolver/SemanticsBuilder.cs
@@ -8,46 +8,82 @@ namespace Semgus.Solver.Rosette {
     class SemanticsBuilder {
         private static readonly CodeTextBuilder _builder = new CodeTextBuilder();
         private readonly static int DEPTH = 4;
+        private static string cond_var = "NO_COND";
 
-        private static void GenerateRuleSteps(IInterpretationStep s, Dictionary<VariableInfo,int> outvars, string[] outvals) {
+        private static void GenerateRuleSteps(CodeTextBuilder builder, IInterpretationStep s, Dictionary<VariableInfo,int> outvars, string[] outvals) {
             switch(s) {
                 case AssignmentFromLocalFormula a_s:
                     int index;
                     if ((index = (outvars.GetValueOrDefault(a_s.ResultVar, -1))) != -1) {
                         outvals[index] = a_s.Expression.Formula.PrintFormula();
                     } else {
-                        _builder.Write($"(define {a_s.ResultVar.Name} {a_s.Expression.Formula.PrintFormula()})");
+                        builder.Write($" (define {a_s.ResultVar.Name} {a_s.Expression.Formula.PrintFormula()})");
                     } 
                     break;
-                case TermEvaluation c_s:
+                case TermEvaluation t_s:
                     _builder.Write(" ");
-                    using (_builder.InParens()) {
-                        _builder.Write("define ");
-                        _builder.Write($"{c_s.OutputVariables[0].Name} ");
-                        using (_builder.InParens()) {
-                            _builder.Write($"{c_s.Term.Nonterminal.Name}.Sem {c_s.Term.Name}");
-                            foreach (VariableInfo in_var in c_s.InputVariables) {
-                                _builder.Write($" {in_var.Name}");
+                    using (builder.InParens()) {
+                        builder.Write("define ");
+                        if (t_s.OutputVariables.Count() == 1) {
+                            if (cond_var == "NO_COND") cond_var = t_s.OutputVariables[0].Name;
+                            builder.Write($"{t_s.OutputVariables[0].Name} ");
+                        } else {
+                            using (builder.InParens()) {
+                                foreach (VariableInfo out_var in t_s.OutputVariables) {
+                                    if ((index = (outvars.GetValueOrDefault(out_var, -1))) != -1) {
+                                        outvals[index] = out_var.Name;
+                                    }
+                                    builder.Write($" {out_var.Name}");
+                                }
+                            }
+                            builder.Write(" ");
+                        }
+                        
+                        using (builder.InParens()) {
+                            builder.Write($"{t_s.Term.Nonterminal.Name}.Sem {t_s.Term.Name}");
+                            foreach (VariableInfo in_var in t_s.InputVariables) {
+                                builder.Write($" {in_var.Name}");
                             }
                         }
                     }
                     break;
-                case ConditionalAssertion:
+                case ConditionalAssertion c_s:
+                    /*foreach (VariableInfo v in c_s.DependencyVariables) {
+                        builder.Write($"{v.Name} ");
+                    }*/
+                    //cond_var = c_s.
                     // unimplemented for now but will be added soon, fall thru for now
+                    break;
                 default:
-                    _builder.Write(" UNIMPLEMENTED");
+                    builder.Write(" UNIMPLEMENTED");
                     break;
             }
         }
 
-        private static void GenerateOutputStatement(string[] outvals) {
+        private static void GenerateOutputStatement(CodeTextBuilder builder, string[] outvals) {
+            builder.Write(" ");
             if (outvals.Count() == 1) {
-                _builder.Write(outvals[0]);
+                builder.Write(outvals[0]);
             } else {
-                _builder.Write(" ");
-                using (_builder.InParens()) {
-                    _builder.Write("list ");
-                    _builder.WriteEach(outvals);
+                builder.Write(" ");
+                using (builder.InParens()) {
+                    builder.Write("values ");
+                    builder.WriteEach(outvals);
+                }
+            }
+        }
+        
+        private static void GenerateSemantics(CodeTextBuilder builder, SemanticRuleInterpreter s, Dictionary<VariableInfo,int> outvars, string[] outvals) {
+            if (s.Steps.Count == 1) {
+                GenerateRuleSteps(builder, s.Steps[0], outvars, outvals);
+                GenerateOutputStatement(builder, outvals);
+            } else {
+                using (builder.InParens()) {
+                    builder.Write("begin");
+                    foreach (IInterpretationStep step in s.Steps) {   
+                        GenerateRuleSteps(builder, step, outvars, outvals);
+                    }
+                    GenerateOutputStatement(builder, outvals);
                 }
             }
         }
@@ -81,6 +117,7 @@ namespace Semgus.Solver.Rosette {
                         _builder.Write("destruct p");
                         foreach (ProductionRuleInterpreter pi in g.Productions[nt]) {
                             string[] outvals = new string[pi.OutputVariables.Count];
+                            cond_var = "NO_COND";
 
                             _builder.LineBreak();
                             using (_builder.InBrackets()) {
@@ -91,18 +128,38 @@ namespace Semgus.Solver.Rosette {
                                     }
                                 }
                                 _builder.Write(" ");
-                                // hack for atomic productions
-                                if (pi.Semantics[0].Steps.Count == 1) {
-                                    GenerateRuleSteps(pi.Semantics[0].Steps[0], outvars, outvals);
-                                    GenerateOutputStatement(outvals);
-                                } else {
-                                    using (_builder.InParens()) {
-                                        _builder.Write("begin");
-                                        foreach (IInterpretationStep step in pi.Semantics[0].Steps) {   
-                                            GenerateRuleSteps(step, outvars, outvals);
+                            
+                                // make assumption that semantics is either one item or
+                                // just two, corresponding to an if/else or loop
+                                
+                                switch (pi.Semantics.Count) {                    
+                                    case 1:
+                                        GenerateSemantics(_builder, pi.Semantics[0], outvars, outvals); break;
+                                    case 2:               
+                                        // Two separate builders for each code block of the if/else or while loop
+                                        CodeTextBuilder if_builder = new CodeTextBuilder();
+                                        CodeTextBuilder else_builder = new CodeTextBuilder();
+
+
+                                        // DANGER!! we are assuming that the first step is the one that sets the cond variable.
+                                        // otherwise you have to do multiple passes. this can be fixed in the future.
+                                        GenerateRuleSteps(_builder, pi.Semantics[0].Steps[0], outvars, outvals);
+                                        GenerateSemantics(if_builder, pi.Semantics[0], outvars, outvals); 
+                                        GenerateSemantics(else_builder, pi.Semantics[1], outvars, outvals); 
+                                        using (_builder.InParens()) {
+                                            _builder.Write($"if ({cond_var})");
+                                            _builder.LineBreak();
+                                            using (_builder.InParens()) {
+                                                _builder.Write(if_builder.ToString());
+                                            }
+                                            _builder.LineBreak();
+                                            using (_builder.InParens()) {
+                                                _builder.Write(else_builder.ToString());
+                                            }
                                         }
-                                        GenerateOutputStatement(outvals);
-                                    }
+                                        break;
+                                    default:
+                                        _builder.Write("UNIMPLEMENTED"); break;
                                 }
                                 
                             }
